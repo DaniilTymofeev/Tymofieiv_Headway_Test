9476a46c69f3a68bd3246c370d4c2ddc6b186b2c
Daniil Tymofeev <tymofeevdaniil.work@gmail.com>
2024-08-11 17:50:41 +0300

    Initial commit
    Initial commit

---
+++ Tymofieiv_Headway_Test/Tymofieiv_Headway_Test/Tymofieiv_Headway_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tymofieiv_Headway_TestApp.swift
+//  Tymofieiv_Headway_Test
+//
+//  Created by Danil Tymofeev on 11.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tymofieiv_Headway_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
