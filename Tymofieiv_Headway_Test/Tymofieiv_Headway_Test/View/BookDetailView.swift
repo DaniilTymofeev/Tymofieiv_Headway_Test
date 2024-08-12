5a9c3216d06e35f473348011b1d9b890a356318a
Daniil Tymofeev <tymofeevdaniil.work@gmail.com>
2024-08-12 21:58:15 +0300

    New UI implementation
    Changed UI to look like Headway App

--- Tymofieiv_Headway_Test/Tymofieiv_Headway_Test/View/BookDetailView.swift
+++ Tymofieiv_Headway_Test/Tymofieiv_Headway_Test/View/BookDetailView.swift
@@ -18,78 +18,75 @@ struct BookDetailView: View {
         GeometryReader { geo in
             VStack {
                 ZStack {
-                    Rectangle()
-                        .frame(width: 200, height: 300)
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(width: geo.size.width*0.55, height: geo.size.height*0.45)
                 }
                 .foregroundStyle(.yellow)
-                .frame(height: geo.size.height/2 + geo.safeAreaInsets.top)
+                .frame(height: geo.size.height/2, alignment: .top)
+                .padding()
                 VStack(spacing: 10) {
                     Text("Key point 3 of 6".uppercased())
                         .bold()
                         .foregroundStyle(.gray)
                         .font(.system(size: 12))
                     Text("Visualize and exercise")
+                        .foregroundStyle(.white)
                     HStack {
                         Text("00:01")
                         Slider(value: $slider, in: 0...100)
                         Text("12:30")
                     }
+                    .foregroundStyle(.gray)
+                    .font(.caption)
                     .padding()
                 
                     Button(action: {}) {
-                        Text("Butt")
+                        Text("1x speed")
                     }
+                    .padding(8)
+                    .background(Color.secondary)
+                    .clipShape(RoundedRectangle(cornerRadius: 4))
+                    .tint(.white)
+                    .font(.system(size: 12, weight: .bold))
+                    
                     Spacer()
+                    
                     HStack(spacing: 40) {
                         Button(action: {}) {
-                            Image(systemName: "play")
+                            Image(systemName: "backward.end.fill")
                         }
+                        .font(.title2)
+                        
                         Button(action: {}) {
-                            Image(systemName: "play")
+                            Image(systemName: "gobackward.5")
                         }
+                        .font(.title)
+                        
                         Button(action: {audioPlayerViewModel.playOrPause()}) {
-                            Image(systemName: "play")
+                            Image(systemName: isShownTextSummary ? "pause.fill" : "play.fill")
                         }
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        
                         Button(action: {}) {
-                            Image(systemName: "play")
+                            Image(systemName: "goforward.10")
                         }
+                        .font(.title)
+                        
                         Button(action: {}) {
-                            Image(systemName: "play")
+                            Image(systemName: "forward.end.fill")
                         }
+                        .font(.title2)
                     }
-                    .background(.yellow)
+                    .tint(.white)
                     Spacer()
                     Toggle(isOn: $isShownTextSummary){}
                         .frame(width: 40, height: 40, alignment: .center)
+                        .tint(.blue)
                 }
             }
-            
-
-//            HStack {
-//                Button(action: {
-//                    viewModel.seekBackward()
-//                }) {
-//                    Image(systemName: "gobackward.5")
-//                }
-//
-//                Button(action: {
-//                    if viewModel.isPlaying {
-//                        viewModel.pauseAudio()
-//                    } else {
-//                        viewModel.playAudio(for: book)
-//                    }
-//                }) {
-//                    Image(systemName: viewModel.isPlaying ? "pause.circle" : "play.circle")
-//                }
-//
-//                Button(action: {
-//                    viewModel.seekForward()
-//                }) {
-//                    Image(systemName: "goforward.10")
-//                }
-//            }
-//            .font(.largeTitle)
         }
+        .background(Color("BackgroundColor", bundle: .main))
         .navigationTitle("book.title")
         .onDisappear {
 //            viewModel.stopAudio()
